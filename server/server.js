e6c6cf30f3ce16a78c2546a0954d5124a41c6fa6
Manveer <manveer7saggu@gmail.com>
2024-09-01 02:46:38 +0530

    defined backend routes for fetching product

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import { scrape } from './scrape.js';
+import cors from "cors";
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
 
 app.get('/scrape', async (req, res) => {
   const url = req.query.url; // Pass the URL as a query parameter
 
   try {
-    scrape(url);
-      } catch (error) {
+    const productData = await scrape(url); // Get the scraped data
+    res.json(productData); // Send the data as a JSON response
+  } catch (error) {
     res.status(500).json({ error: 'Failed to scrape the data' });
   }
 });
