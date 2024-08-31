a3251edf05a2441eed1b61365a15c9c2daedb6ac
Manveer <manveer7saggu@gmail.com>
2024-09-01 02:46:57 +0530

    ratings

--- client_new/src/App.js
+++ client_new/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Product from "./components/Product"
 import Input from "./components/Input"
 export default function App() {
-  const [product, setProduct] = useState({ title: '', imgURL: '', priceTxt: '' });
+  const [product, setProduct] = useState({ title: '', imgURL: '', priceTxt: '', ratingTxt: '' });
 
   const handleProductData = (data) => {
     setProduct(data);
