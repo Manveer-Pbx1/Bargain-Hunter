cc33ca4998de38a4611ee5aad44c007868ba41ab
Manveer <manveer7saggu@gmail.com>
2024-09-07 03:19:12 +0530

    Defined Paths

--- client_new/src/App.js
+++ client_new/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import Home from './components/Home'
 import LandingPage from './components/LandingPage'
-
+import SignUp from './components/SignUp';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './components/Home';
 export default function App() {
   return (
-    <div>
-      {/* <Home/> */}
-      <LandingPage/>
-    </div>
+   <Router>
+    <Routes>
+      <Route path= "/" element = {<LandingPage/>} />
+      <Route path= "/signup" element = {<SignUp/>}/>
+      <Route path= "/home" element = {<Home/>}/>
+    </Routes>
+   </Router>
   )
 }
