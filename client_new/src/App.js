57fafe41e1d3a5efbea56c948c942dc8369f47cf
Manveer <manveer7saggu@gmail.com>
2024-09-07 22:43:13 +0530

    defined protected route for Home

--- client_new/src/App.js
+++ client_new/src/App.js
@@ -4,6 +4,7 @@ import SignUp from './components/SignUp';
 import Login from './components/Login';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './components/Home';
+import ProtectedRoute from './components/ProtectedRoute';
 export default function App() {
   return (
    <Router>
@@ -11,7 +12,7 @@ export default function App() {
       <Route path= "/" element = {<LandingPage/>} />
       <Route path= "/signup" element = {<SignUp/>}/>
       <Route path= "/login" element = {<Login/>}/>
-      <Route path= "/home" element = {<Home/>}/>
+      <Route path= "/home" element = {<ProtectedRoute element={<Home/>}/>}/>
     </Routes>
    </Router>
   )
