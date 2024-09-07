44006683d113cccd76ed36d7083640bebf114533
Manveer <manveer7saggu@gmail.com>
2024-09-07 22:21:49 +0530

    new route: login

--- client_new/src/App.js
+++ client_new/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import LandingPage from './components/LandingPage'
 import SignUp from './components/SignUp';
+import Login from './components/Login';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './components/Home';
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <Routes>
       <Route path= "/" element = {<LandingPage/>} />
       <Route path= "/signup" element = {<SignUp/>}/>
+      <Route path= "/login" element = {<Login/>}/>
       <Route path= "/home" element = {<Home/>}/>
     </Routes>
    </Router>
