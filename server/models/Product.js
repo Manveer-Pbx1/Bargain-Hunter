a43ada509d5e1707bfa06d1c1d64e63af44aba98
Manveer <manveer7saggu@gmail.com>
2024-09-10 01:43:19 +0530

    [contains bugs] feat: saving products associated with users

---
+++ server/models/Product.js
@@ -0,0 +1,28 @@
+// models/Product.js
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: String,
+    required: true,
+  },
+  imgURL: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
--- server/models/User.js
+++ server/models/User.js
@@ -1,39 +1,26 @@
+// models/User.js
 const mongoose = require('mongoose');
-
+const Product = require('./Product.js')
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        minlength: 5,
-        maxlength: 20
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+\@.+\..+/, 'Please enter a valid email address']
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
-    updatedAt: {
-        type: Date
-    },
-})
-
-userSchema.pre('save', function(next){
-    this.updatedAt = Date.now(); 
-    next();
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  products: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  }],
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
